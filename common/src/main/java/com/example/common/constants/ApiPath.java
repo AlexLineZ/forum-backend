4a9028ca1182aba518e017939c560c1335ed1c2a
Aleksandr Kirsanov <alex.kirsanov182@gmail.com>
2024-04-19 00:02:11 +0700

    Удален лишний файл

--- common/src/main/java/com/example/common/constants/ApiPath.java
+++
@@ -1,7 +0,0 @@
-package com.example.common.constants;
-
-public class ApiPath {
-    public static final String BASE_API = "/api";
-    public static final String USER = BASE_API + "/users";
-    public static final String ADMIN = BASE_API + "/admin";
-}
